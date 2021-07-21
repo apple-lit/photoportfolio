f7a7db43fbe1c3fc588e7eaf361a16dcc76950c5
Rintaro Kudo <rinta.kudo@gmail.com>
2021-07-21 22:45:08 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+    $(document).ready(function(){
+      $('.slideshow').bxSlider({
+        pager: false,
+        controls: false,
+        auto: true,
+        mode: 'fade',
+        speed: 2000,
+        pause:8000
+      });
+    });
