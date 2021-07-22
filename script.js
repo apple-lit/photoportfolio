c1507a56d1a604fb42306eaefb295a9769e7b961
Rintaro Kudo <rinta.kudo@gmail.com>
2021-07-23 08:54:22 +0900

    other pages added w/ coming soon

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-    $(document).ready(function(){
+$(document).ready(function () {
+      $('body').fadeIn(2000);
       $('.slideshow').bxSlider({
         pager: false,
         controls: false,
         auto: true,
         mode: 'fade',
         speed: 2000,
-        pause:8000
+        pause: 8000,
+        randomStart: true
       });
     });
